4591cd27ec100b9a013d1ddb934358638bf9f8cd
Neksov <n2v@list.ru>
2020-08-22 14:48:11 +0300

    new work

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var list = $(".cards .card");
+  var numToShow = 6; //сколько показывать элементов
+  var button = $("button");
+  var numInList = list.length;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
+  button.click(function () {
+    var showing = list.filter(':visible').length;
+    list.slice(showing - 1, showing + numToShow).fadeIn();
+    var nowShowing = list.filter(':visible').length;
+    if (nowShowing >= numInList) {
+      button.hide();
+    }
+  });
+});
