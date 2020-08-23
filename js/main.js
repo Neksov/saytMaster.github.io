28bcbf77bc441566605d4148940007b08f4c8dae
Neksov <n2v@list.ru>
2020-08-23 13:44:59 +0300

    add new block

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var list = $(".cards .card");
   var numToShow = 6; //сколько показывать элементов
-  var button = $("button");
+  var button = $(".btn");
   var numInList = list.length;
   list.hide();
   if (numInList > numToShow) {
@@ -16,4 +16,30 @@ $(document).ready(function () {
       button.hide();
     }
   });
+});
+
+function DropDown(el) {
+  this.dd = el;
+  this.initEvents();
+}
+DropDown.prototype = {
+  initEvents: function () {
+    var obj = this;
+
+    obj.dd.on('click', function (event) {
+      $(this).toggleClass('active');
+      event.stopPropagation();
+    });
+  }
+}
+
+$(function () {
+
+  var distance = new DropDown($('#distance'));
+
+  $(document).click(function () {
+    // all dropdowns
+    $('.wrapper-dropdown-2').removeClass('active');
+  });
+
 });
